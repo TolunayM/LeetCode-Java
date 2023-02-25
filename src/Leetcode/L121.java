f58030c5c3d36f31c0b32ea59c445157039a30fb
Tolunay Mutlu <tlnymtlu@gmail.com>
2023-02-25 20:58:43 +0300

    new file

--- src/Leetcode/L121.java
+++ src/Leetcode/L121.java
@@ -1,2 +1,51 @@
-package Leetcode;public class L121 {
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//Best Time to Buy and Sell Stock - Easy
+public class L121 {
+
+    public int maxProfitV2(int[] prices){
+        int profit = 0;
+        int price = 0;
+        if(prices.length < 2){
+            return 0;
+        }else{
+
+            for(int j = prices.length - 1; j > 0; j--){
+                for(int i = 0; i < j; i++){
+                    price = prices[j] - prices[i];
+                    if(price > profit){
+                        profit = price;
+                    }
+                }
+            }}
+        return (profit);
+    }
+
+
+    public int maxProfit(int[] prices) {
+        List<Integer> profitList = new ArrayList<>();
+        // Check if can achieve profit
+        if(prices.length < 2){
+            return 0;
+        }
+        int profit = 0;
+        for(int j = prices.length - 1; j > 0; j--){
+            for(int i = 0; i < j; i++){
+
+                profit = prices[j] - prices[i];
+                profitList.add(profit);
+            }
+        }
+        Collections.sort(profitList);
+        System.out.println(profitList.get(profitList.size()- 1) > 0 ? profitList.get(profitList.size()- 1):0);
+        return (profitList.get(profitList.size()- 1) > 0 ? profitList.get(profitList.size()- 1):0);
+    }
+    public static void main(String[] args) {
+        L121 obj = new L121();
+        obj.maxProfit(new int[]{1});
+    }
 }
--- src/Leetcode/L20.java
+++ src/Leetcode/L20.java
@@ -1,2 +1,28 @@
-package Leetcode;public class L20 {
+package Leetcode;
+
+import java.util.Stack;
+
+//Valid Parentheses - Easy
+public class L20 {
+    public boolean isValid(String s) {
+
+        Stack<Character> charStack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == ')' && !charStack.isEmpty() && charStack.peek() == '('){
+                charStack.pop();
+            }else if(c == ']' && !charStack.isEmpty() && charStack.peek() == '['){
+                charStack.pop();
+            }else if(c == '}' && !charStack.isEmpty() && charStack.peek() == '{'){
+                charStack.pop();
+            }else{
+                charStack.push(c);
+            }
+        }
+
+        return charStack.isEmpty();
+    }
+    public static void main(String[] args) {
+        L20 obj = new L20();
+        obj.isValid("()[]{}");
+    }
 }
